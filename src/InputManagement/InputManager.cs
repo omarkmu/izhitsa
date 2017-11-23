f40065b0139a2e3641b56ddceafdfe5bd25dd9a3
Omar M <omarkmu@users.noreply.github.com>
2017-11-23 05:03:42 -0500

    Update references

--- src/InputManagement/InputManager.cs
+++ src/InputManagement/InputManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Izhitsa.EventManagement.EventManager;
+using Izhitsa.EventManagement;
 
 namespace Izhitsa {
 	namespace InputManagement {
--- src/InputManagement/InputManager_Events.cs
+++ src/InputManagement/InputManager_Events.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using static Izhitsa.EventManagement.EventManager;
+using Izhitsa.EventManagement;
 
 namespace Izhitsa {
 	namespace InputManagement {
