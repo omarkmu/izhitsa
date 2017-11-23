17162ef41eaca2c3e1ac2ae1e86ac016e8463a62
Omar M <omarkmu@users.noreply.github.com>
2017-11-23 05:02:07 -0500

    Move Broadcast & Signal
    Move Broadcast and Signal from EventManager to EventManagement

--- src/EventManagement/Broadcast.cs
+++ src/EventManagement/Broadcast.cs
@@ -5,127 +5,125 @@ using static Izhitsa.Tasks.TaskUtils;
 
 namespace Izhitsa {
 	namespace EventManagement {
-		public static partial class EventManager {
+		/**
+		 * <summary>Class which represents an event.</summary>
+		 */
+		public class Broadcast {
+			/// <summary>The name of the Broadcast. (Read Only)</summary>
+			public string Name { get; private set; }
+
+			private List<Signal> signals = new List<Signal>();
+
+
 			/**
-			 * <summary>Class which represents an event.</summary>
+			 * <summary>
+			 * Creates a nameless, empty Broadcast and does not
+			 * register it with the EventManager.
+			 * </summary>
 			 */
-			public class Broadcast {
-				/// <summary>The name of the Broadcast. (Read Only)</summary>
-				public string Name { get; private set; }
-
-				private List<Signal> signals = new List<Signal>();
-
-
-				/**
-				 * <summary>
-				 * Creates a nameless, empty Broadcast and does not
-				 * register it with the EventManager.
-				 * </summary>
-				 */
-				public Broadcast(){}
-				/**
-				 * <summary>
-				 * Creates a new Broadcast.
-				 * </summary>
-				 * <param name="name">The name of the Broadcast, for `<see cref="EventManager"/>` registering.
-				 * </param>
-				 * <param name="unregistered">If this is true, the Broadcast will not be registered with
-				 * `<see cref="EventManager"/>`.
-				 * </param>
-				 */
-				public Broadcast(string name, bool unregistered = false){
-					Name = name;
-					if (!unregistered){
-						if (GetBroadcast(name) != null)
-							throw new ArgumentException($"{name} is already a registered Broadcast.", "name");
-						register(this);
-					}
+			public Broadcast(){}
+			/**
+			 * <summary>
+			 * Creates a new Broadcast.
+			 * </summary>
+			 * <param name="name">The name of the Broadcast, for `<see cref="EventManager"/>` registering.
+			 * </param>
+			 * <param name="unregistered">If this is true, the Broadcast will not be registered with
+			 * `<see cref="EventManager"/>`.
+			 * </param>
+			 */
+			public Broadcast(string name, bool unregistered = false){
+				Name = name;
+				if (!unregistered){
+					if (EventManager.GetBroadcast(name) != null)
+						throw new ArgumentException($"{name} is already a registered Broadcast.", "name");
+					EventManager.register(this);
 				}
+			}
+			
+			/**
+			 * <summary>
+			 * Fires the Broadcast.
+			 * </summary>
+			 * <param name="args">Arguments to fire with.</param>
+			 */
+			public void Fire(params object[] args){
+				for (int i = 0; i < signals.Count; i++)
+					StartCoroutine(fire(signals[i], args));
+			}
+			/**
+			 * <summary>
+			 * Connects an Action and returns a `<see cref="Signal"/>`.
+			 * </summary>
+			 * <param name="func">The Action to connect.
+			 * </param>
+			 */
+			public Signal Connect(Action<object[]> func){
+				Signal s = new Signal(func, this);
+				signals.Add(s);
+				return s;
+			}
+			/**
+			 * <summary>
+			 * Connects an Action, and returns a `<see cref="Signal"/>`.
+			 * </summary>
+			 * <param name="func">The Action to connect.
+			 * </param>
+			 */
+			public Signal Connect(Action func) => Connect((args) => func());
+			
+			/**
+			 * <summary>
+			 * Connects an Action to the Broadcast.
+			 * </summary>
+			 * <param name="bc">The Broadcast.
+			 * </param>
+			 * <param name="func">The Action to connect.
+			 * </param>
+			 */
+			public static Broadcast operator +(Broadcast bc, Action<object[]> func){
+				bc.Connect(func);
+				return bc;
+			}
+			/**
+			 * <summary>
+			 * Disconnects an Action from the Broadcast.
+			 * </summary>
+			 * <param name="bc">The Broadcast.
+			 * </param>
+			 * <param name="func">The Action to disconnect.
+			 * </param>
+			 */
+			public static Broadcast operator -(Broadcast bc, Action<object[]> func){
+				List<Signal> toRemove = new List<Signal>();
+				foreach (Signal s in bc.signals)
+					if (s.callback == func) toRemove.Add(s);
+				foreach(Signal s in toRemove) bc.disconnect(s);
 				
-				/**
-				 * <summary>
-				 * Fires the Broadcast.
-				 * </summary>
-				 * <param name="args">Arguments to fire with.</param>
-				 */
-				public void Fire(params object[] args){
-					for (int i = 0; i < signals.Count; i++)
-						StartCoroutine(fire(signals[i], args));
-				}
-				/**
-				 * <summary>
-				 * Connects an Action and returns a `<see cref="Signal"/>`.
-				 * </summary>
-				 * <param name="func">The Action to connect.
-				 * </param>
-				 */
-				public Signal Connect(Action<object[]> func){
-					Signal s = new Signal(func, this);
-					signals.Add(s);
-					return s;
-				}
-				/**
-				 * <summary>
-				 * Connects an Action, and returns a `<see cref="Signal"/>`.
-				 * </summary>
-				 * <param name="func">The Action to connect.
-				 * </param>
-				 */
-				public Signal Connect(Action func) => Connect((args) => func());
-				
-				/**
-				 * <summary>
-				 * Connects an Action to the Broadcast.
-				 * </summary>
-				 * <param name="bc">The Broadcast.
-				 * </param>
-				 * <param name="func">The Action to connect.
-				 * </param>
-				 */
-				public static Broadcast operator +(Broadcast bc, Action<object[]> func){
-					bc.Connect(func);
-					return bc;
-				}
-				/**
-				 * <summary>
-				 * Disconnects an Action from the Broadcast.
-				 * </summary>
-				 * <param name="bc">The Broadcast.
-				 * </param>
-				 * <param name="func">The Action to disconnect.
-				 * </param>
-				 */
-				public static Broadcast operator -(Broadcast bc, Action<object[]> func){
-					List<Signal> toRemove = new List<Signal>();
-					foreach (Signal s in bc.signals)
-						if (s.callback == func) toRemove.Add(s);
-					foreach(Signal s in toRemove) bc.disconnect(s);
-					
-					return bc;
-				}
+				return bc;
+			}
 
-				/**
-				 * <summary>
-				 * Disconnects a `<see cref="Signal"/>`.
-				 * </summary>
-				 * <param name="sig">The Signal to disconnect.</param>
-				 */
-				internal void disconnect(Signal sig){
-					if (signals.Contains(sig)) signals.Remove(sig);
-				}
-				
-				/**
-				 * <summary>
-				 * Fires a `<see cref="Signal"/>`.
-				 * </summary>
-				 * <param name="sig">`<see cref="Signal"/>` to fire.
-				 * </param>
-				 * <param name="args">Arguments to fire the `<see cref="Signal"/>` with.</param>
-				 */
-				private IEnumerator fire(Signal sig, params object[] args){
-					sig.call(args);
-					yield return null;
-				}
+			/**
+			 * <summary>
+			 * Disconnects a `<see cref="Signal"/>`.
+			 * </summary>
+			 * <param name="sig">The Signal to disconnect.</param>
+			 */
+			internal void disconnect(Signal sig){
+				if (signals.Contains(sig)) signals.Remove(sig);
+			}
+			
+			/**
+			 * <summary>
+			 * Fires a `<see cref="Signal"/>`.
+			 * </summary>
+			 * <param name="sig">`<see cref="Signal"/>` to fire.
+			 * </param>
+			 * <param name="args">Arguments to fire the `<see cref="Signal"/>` with.</param>
+			 */
+			private IEnumerator fire(Signal sig, params object[] args){
+				sig.call(args);
+				yield return null;
 			}
 		}
 	}
--- src/EventManagement/Signal.cs
+++ src/EventManagement/Signal.cs
@@ -2,82 +2,80 @@ using System;
 
 namespace Izhitsa {
 	namespace EventManagement {
-		public static partial class EventManager {
+		/**
+		 * <summary>
+		 * Class which contains a callback for use in `<see cref="Broadcast"/>`.
+		 * </summary>
+		 * <seealso cref="EventManager.Broadcast"/>
+		 */
+		public class Signal {
 			/**
 			 * <summary>
-			 * Class which contains a callback for use in `<see cref="Broadcast"/>`.
+			 * The `<see cref="EventManager.Broadcast"/>` that this Signal is connected to. (Read Only)
 			 * </summary>
-			 * <seealso cref="EventManager.Broadcast"/>
+			 * <exception cref="MemberAccessException">
+			 * Thrown if an attempt to access the member is made
+			 * after the signal has been disconnected.
+			 * </exception>
 			 */
-			public class Signal {
-				/**
-				 * <summary>
-				 * The `<see cref="EventManager.Broadcast"/>` that this Signal is connected to. (Read Only)
-				 * </summary>
-				 * <exception cref="MemberAccessException">
-				 * Thrown if an attempt to access the member is made
-				 * after the signal has been disconnected.
-				 * </exception>
-				 */
-				public Broadcast Broadcast {
-					get {
-						if (Disconnected)
-							throw new MemberAccessException("Cannot access member; " +
-							"the signal is disconnected.");
-						return broadcast;
-					}
-					internal set { broadcast = value; }
-				}
-				/// <summary>Is this signal disconnected? (Read Only)</summary>
-				public bool Disconnected { get; private set; }
-				/// <summary>The signal's callback function.</summary>
-				internal Action<object[]> callback { get; set; }
-
-				/// <summary>The `<see cref="EventManager.Broadcast"/>` this Signal is connected to.</summary>
-				private Broadcast broadcast;
-				
-				
-				/**
-				 * <summary>
-				 * Creates a Signal.
-				 * </summary>
-				 * <param name="func">A callback function to connect to the Signal.
-				 * </param>
-				 * <param name="bc">The `<see cref="EventManager.Broadcast"/>`
-				 * that the Signal is being added to.</param>
-				 */
-				internal Signal(Action<object[]> func, Broadcast bc){
-					callback = func;
-					broadcast = bc;
-				}
-
-				/**
-				 * <summary>
-				 * Disconnects the Signal from its Broadcast.
-				 * </summary>
-				 * <exception cref="MethodAccessException">
-				 * Thrown if the Signal is already disconnected.
-				 * </exception>
-				 */
-				public void Disconnect(){
+			public Broadcast Broadcast {
+				get {
 					if (Disconnected)
-						throw new MethodAccessException("The signal is already disconnected.");
-					broadcast?.disconnect(this);
-					Disconnected = true;
-					Broadcast = null;
-					callback = null;
+						throw new MemberAccessException("Cannot access member; " +
+						"the signal is disconnected.");
+					return broadcast;
 				}
-				
-				/**
-				 * <summary>
-				 * Calls the callback function.
-				 * </summary>
-				 * <param name="args">Arguments to fire the Signal with.</param>
-				 */
-				internal void call(params object[] args) {
-					if (callback == null) return;
-					callback(args);
-				}
+				internal set { broadcast = value; }
+			}
+			/// <summary>Is this signal disconnected? (Read Only)</summary>
+			public bool Disconnected { get; private set; }
+			/// <summary>The signal's callback function.</summary>
+			internal Action<object[]> callback { get; set; }
+
+			/// <summary>The `<see cref="EventManager.Broadcast"/>` this Signal is connected to.</summary>
+			private Broadcast broadcast;
+			
+			
+			/**
+			 * <summary>
+			 * Creates a Signal.
+			 * </summary>
+			 * <param name="func">A callback function to connect to the Signal.
+			 * </param>
+			 * <param name="bc">The `<see cref="EventManager.Broadcast"/>`
+			 * that the Signal is being added to.</param>
+			 */
+			internal Signal(Action<object[]> func, Broadcast bc){
+				callback = func;
+				broadcast = bc;
+			}
+
+			/**
+			 * <summary>
+			 * Disconnects the Signal from its Broadcast.
+			 * </summary>
+			 * <exception cref="MethodAccessException">
+			 * Thrown if the Signal is already disconnected.
+			 * </exception>
+			 */
+			public void Disconnect(){
+				if (Disconnected)
+					throw new MethodAccessException("The signal is already disconnected.");
+				broadcast?.disconnect(this);
+				Disconnected = true;
+				Broadcast = null;
+				callback = null;
+			}
+			
+			/**
+			 * <summary>
+			 * Calls the callback function.
+			 * </summary>
+			 * <param name="args">Arguments to fire the Signal with.</param>
+			 */
+			internal void call(params object[] args) {
+				if (callback == null) return;
+				callback(args);
 			}
 		}
 	}
